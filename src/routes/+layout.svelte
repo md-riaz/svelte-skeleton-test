195bf0c1a905acc345a8bbc5ca504f5ae772518c
md-riaz <riazmd582@gmail.com>
2024-08-22 23:20:13 +0600

    chore: Update layout component with navigation message old

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,16 @@
 <script>
 	import { navigating } from '$app/stores';
+	import Loader from '$lib/components/loader.svelte';
+	import { loading } from '$lib/store/loadingStore';
 	import { setUserState } from '$lib/store/state.svelte';
-	import { fade } from 'svelte/transition';
 	import '../app.css';
 
 	const user = setUserState({ name: 'mdriaz' });
 
-	// $: loading.setNavigate(!!$navigating);
-	// $: loading.setLoading(!!$navigating, 'Loading, please wait...');
+	$: loading.setNavigate(!!$navigating);
+	$: loading.setLoading(!!$navigating, 'Loading, please wait...');
 </script>
 
-<!-- <Loader /> -->
-
-{#if $navigating}
-	<!-- content here -->
-	<span class="navigating" in:fade={{ delay: 1000, duration: 150 }} out:fade={{duration:150}}>
-	Navigating to {$navigating?.to?.url.pathname}	
-	</span>
-{/if}
+<Loader />
 
 <slot></slot>
