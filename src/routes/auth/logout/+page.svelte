2c437edb64b6ba69221abf99ed9b7df8fb95a5ff
MD Riaz <mdriaz.wd@gmail.com>
2024-11-12 10:18:51 +0600

    Implement logout functionality by clearing the Dexie database and navigating to the login page

---
+++ src/routes/auth/logout/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { goto } from "$app/navigation";
+	import { db } from "$lib/db";
+
+
+// implement logout like clearing dexie db and navigating to /auth/login
+db.delete();
+
+// navigate to /auth/login
+goto("/auth/login");
+</script>
