a33e0aeb48621b279e877f9ccd75ba8ba93caf26
MD Riaz <mdriaz.wd@gmail.com>
2024-11-12 14:46:31 +0600

    Add service worker push notification subscription on mount

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-
 	import { page } from '$app/stores';
 
 	import { navigating } from '$app/stores';
@@ -9,6 +8,7 @@
 
 	import { variables } from '$lib/utils/constants';
 	import { ToastProvider } from '@skeletonlabs/skeleton-svelte';
+	import { onMount } from 'svelte';
 	import '../app.css';
 
 	/** @type {{children?: import('svelte').Snippet}} */
@@ -21,7 +21,31 @@
 		loading.setLoading(!!$navigating, 'Loading, please wait...');
 	});
 
+	onMount(async () => {
+		if ('serviceWorker' in navigator) {
+			// Service worker supported
 
+			const status = await Notification.requestPermission();
+
+			if (status !== 'granted') {
+				alert('Please allow notifications to make sure that the application works.');
+			}
+
+			const reg = await navigator.serviceWorker.ready;
+			let sub = await reg.pushManager.getSubscription();
+			if (!sub) {
+				const strAppPublicKey =
+					'BJthRQ5myDgc7OSXzPCMftGw-n16F7zQBEN7EUD6XxcfTTvrLGWSIG7y_JxiWtVlCFua0S8MTB5rPziBqNx1qIo';
+				
+				sub = await reg.pushManager.subscribe({
+					userVisibleOnly: true,
+					applicationServerKey: strAppPublicKey
+				});
+			}
+			console.log(sub);
+		}
+		// ...
+	});
 </script>
 
 <svelte:head>
