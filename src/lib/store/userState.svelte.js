62c11ab7f39cc2a1f43ee2c3dfae6d468d2117fa
MD Riaz <mdriaz.wd@gmail.com>
2025-01-02 20:55:23 +0600

    Add user type definitions and initialize user info fields in user state

--- src/lib/store/userState.svelte.js
+++ src/lib/store/userState.svelte.js
@@ -1,8 +1,35 @@
+/**
+ * @typedef {Object} User
+ * @property {string|null} auth_token
+ * @property {Services} services
+ * @property {Settings} settings
+ * @property {Info} info
+ */
 
+/**
+ * @typedef {Object} Services
+ * // Add more services as needed
+ */
+
+/**
+ * @typedef {Object} Settings
+ * // Add more settings as needed
+ */
+
+/**
+ * @typedef {Object} Info
+ * @property {string} [name]
+ * @property {string} [email]
+ * // Add more info fields as needed
+ */
+
+/** @type {User} */
 export const user = $state({
     auth_token: null,
     services: {},
     settings: {},
-    info: {},
+    info: {
+        name: '',
+        email: '',
+    },
 });
-
